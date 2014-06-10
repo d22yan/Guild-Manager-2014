7f352b8ba174fd0607711b27aba8db31ead3e4e7
Dann <yan4646@hotmail.com>
2014-06-10 18:48:39 -0400

    Monster clicked.

---
+++ Assets/MonsterScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseUp() {
+		Debug.Log ("monster clicked");
+	}
+}
