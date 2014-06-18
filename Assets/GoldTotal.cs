71434c6377eee667549fbc12a7308afa2fc8ea73
Dann <yan4646@hotmail.com>
2014-06-18 18:32:48 -0400

    Gold displayed.

---
+++ Assets/GoldTotal.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoldTotal : MonoBehaviour {
+
+	public GUIStyle style1;
+	public GameState gameState;
+
+	void OnGUI() {
+		GUI.Label(new Rect(120, 10, 100, 20), gameState.PlayerGold.ToString(), style1);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/PlayerDefeated.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDefeated : MonoBehaviour {
+
+	public PlayerStatus playerStatus;
+	public GameState gameState;
+	public PlayerSpawn playerSpawn;
+
+
+	// Use this for initialization
+	void Start () {
+		//gameState = GameObject.Find ("Main Camera").GetComponent<GameState> ();
+		 //= GameObject.Find ("Main Camera").GetComponent<MonsterSpawn> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
