0cb4dcf85ce809fb433e4cff8403474c7794df03
Lewis Liu <develop@lewisl.net>
2014-06-21 16:32:50 -0400

    -added alternative fix for #9

--- Assets/MonsterAttacked.cs
+++ Assets/MonsterAttacked.cs
@@ -7,38 +7,33 @@ public class MonsterAttacked : MonoBehaviour {
 	public MonsterStatus monsterStatus;
 	public MonsterDefeated monsterDefeated;
 
-    private bool IsAttackingMonster;
-
 	// Use this for initialization
 	void Start () {
-        IsAttackingMonster = false;
-        InvokeRepeating(
-            "AttackedByGuildMembers",
-            Constant.DelayGuildMemberInitialAttack,
-            GameState.State.PlayerStatus.GuildStatus.Mage.GetPassiveRate() + GameState.State.PlayerStatus.GuildStatus.Archer.GetPassiveRate()
-        );
+        InvokeRepeating("AttackedByMage", 0, GameState.State.PlayerStatus.GuildStatus.Mage.GetPassiveRate());
+        InvokeRepeating("AttackedByArcher", 0, GameState.State.PlayerStatus.GuildStatus.Archer.GetPassiveRate());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        DeathCheck();
 	}
 
 	void OnMouseUp() {
+		Debug.Log ("monster clicked");
 		DealDamage (GameState.State.PlayerStatus.getAttack());
 	}
 
 	void DealDamage(int damage) {
 		monsterStatus.CurrentHealth -= damage;
-        DeathCheck();
 	}
-
-    void AttackedByGuildMembers()
+    void AttackedByMage()
     {
         monsterStatus.CurrentHealth -= GameState.State.PlayerStatus.GuildStatus.Mage.GetPassiveStat();
-        DeathCheck();
     }
-
+    void AttackedByArcher()
+    {
+        monsterStatus.CurrentHealth -= GameState.State.PlayerStatus.GuildStatus.Archer.GetPassiveStat();
+    }
     void DeathCheck()
     {
         if (monsterStatus.CurrentHealth < 1)
