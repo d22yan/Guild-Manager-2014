4155ca9ded4bc98e16afd818f3f5428fedc3332b
sanghoon <sh42lee@uwaterloo.ca>
2014-06-22 22:53:37 -0400

    modified position of monster and player's hp bars

--- Assets/MonsterHPBar.cs
+++ Assets/MonsterHPBar.cs
@@ -5,19 +5,20 @@ public class MonsterHPBar : MonoBehaviour {
 
 	public MonsterStatus monsterStatus;
 	float barDisplay = 1;
-	Vector2 pos = new Vector2(20,40);
-	Vector2 size = new Vector2(60,20);
+	int barWidth = (Screen.width/5);
+	Vector2 pos = new Vector2((Screen.width/5)*2,Screen.height/4);
+	Vector2 size = new Vector2((Screen.width/5)*3,20);
 	public Texture2D progressBarEmpty;
 	public Texture2D progressBarFull;
 
 	void OnGUI() {
 		// draw the background:
 		GUI.BeginGroup (new Rect (pos.x, pos.y, size.x, size.y));
-		GUI.Box (new Rect (0,0, size.x, size.y),progressBarEmpty);
+		GUI.Box (new Rect (0,0, barWidth, size.y),progressBarEmpty);
 		
 		// draw the filled-in part:
 		GUI.BeginGroup (new Rect (0, 0, size.x * barDisplay, size.y));
-		GUI.Box (new Rect (0,0, size.x, size.y),progressBarFull);
+		GUI.Box (new Rect (0,0, barWidth, size.y),progressBarFull);
 		GUI.EndGroup ();
 		
 		GUI.EndGroup ();
