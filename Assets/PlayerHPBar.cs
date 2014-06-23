27e794033a3a6dfc0b2a429b5e82bb1387eec00d
sanghoon <sh42lee@uwaterloo.ca>
2014-06-22 23:15:22 -0400

    refactored

--- Assets/PlayerHPBar.cs
+++ Assets/PlayerHPBar.cs
@@ -4,20 +4,19 @@ using System.Collections;
 public class PlayerHPBar : MonoBehaviour {
 
 	float barDisplay = 1;
-	int barWidth = (Screen.width/4) * 2;
 	Vector2 pos = new Vector2((Screen.width/4),Screen.height-23);
-	Vector2 size = new Vector2((Screen.width/4)*3,20);
+	Vector2 size = new Vector2((Screen.width/4) * 2,20);
 	public Texture2D progressBarEmpty;
 	public Texture2D progressBarFull;
 	
 	void OnGUI() {
 		// draw the background:
 		GUI.BeginGroup (new Rect (pos.x, pos.y, size.x, size.y));
-		GUI.Box (new Rect (0,0, barWidth, size.y), progressBarEmpty);
+		GUI.Box (new Rect (0,0, size.x, size.y), progressBarEmpty);
 		
 		// draw the filled-in part:
-		GUI.BeginGroup (new Rect (0, 0, barWidth * barDisplay, size.y));
-		GUI.Box (new Rect (0,0, barWidth, size.y), progressBarFull);
+		GUI.BeginGroup (new Rect (0, 0, size.x * barDisplay, size.y));
+		GUI.Box (new Rect (0,0, size.x, size.y), progressBarFull);
 		GUI.EndGroup ();
 		
 		GUI.EndGroup ();
