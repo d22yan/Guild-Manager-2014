464455dd94d267f42764fbe479bf6029f6924c46
Dann <yan4646@hotmail.com>
2014-06-23 13:25:23 -0400

    Implemented Display Damage - red text with damage value is displayed on the mouse position when click event is triggered - displays damage when guild member attacks also

---
+++ Assets/DisplayDamage.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayDamage : MonoBehaviour {
+
+    private float CreationTime;
+    private float TimeToFade;
+
+	// Use this for initialization
+	void Start () {
+        CreationTime = Time.time;
+        TimeToFade = 1;      
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(0.001f, 0.001f, 0f));
+        Utility.SetAlpha(guiText.material, Mathf.Cos((Time.time - CreationTime) * ((Mathf.PI / 2) / TimeToFade)));
+        Destroy(gameObject, TimeToFade);
+	}
+}
---
+++ Assets/Utility.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Utility {
+    public static void SetAlpha(this Material material, float value) {
+        Color color = material.color;
+        color.a = value;
+        material.color = color;
+    }
+}
