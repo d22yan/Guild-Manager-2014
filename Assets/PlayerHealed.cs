83c1647c80eb5931ebb90c5db166b65d2ac91cd0
Lewis Liu <develop@lewisl.net>
2014-06-23 22:58:57 -0400

    -polished healing effect and number display

--- Assets/PlayerHealed.cs
+++ Assets/PlayerHealed.cs
@@ -3,9 +3,15 @@ using System.Collections;
 
 public class PlayerHealed : MonoBehaviour {
     public Transform HealingEffect;
+    public GameObject HealingBox;
+
+    private Vector2 InitialScale;
+    private Vector2 InitialPosition;
 
 	// Use this for initialization
 	void Start () {
+        InitialScale = new Vector2(transform.localScale.x, transform.localScale.y);
+        InitialPosition = new Vector2(transform.position.x, transform.position.y);
 		InvokeRepeating("HealedByPaladin", GameState.State.PlayerStatus.GuildStatus.Paladin.GetPassiveRate(),GameState.State.PlayerStatus.GuildStatus.Paladin.GetPassiveRate());
 		InvokeRepeating("HealedByPriest", GameState.State.PlayerStatus.GuildStatus.Priest.GetPassiveRate(),GameState.State.PlayerStatus.GuildStatus.Priest.GetPassiveRate());
 	}
@@ -29,16 +35,30 @@ public class PlayerHealed : MonoBehaviour {
         Destroy(healingEffect.gameObject, 2);
     }
 
+    void DisplayHealing(int Healing, Vector2 InputPosition)
+    {
+        GameObject boxClone = (GameObject)Instantiate(HealingBox, new Vector2(InputPosition.x / Screen.width, InputPosition.y / Screen.height), new Quaternion());
+        boxClone.guiText.text = "+" + Healing;
+    }
+
     void HealedByPaladin()
     {
-        GameState.State.PlayerStatus.CurrentHealth += GameState.State.PlayerStatus.GuildStatus.Paladin.GetPassiveStat();
-        if (GameState.State.PlayerStatus.GuildStatus.Paladin.GetPassiveStat() > 0)
+        var healAmount = GameState.State.PlayerStatus.GuildStatus.Paladin.GetPassiveStat();
+        if (healAmount > 0)
+        {
+            GameState.State.PlayerStatus.CurrentHealth += healAmount;
             DisplayHealingEffect();
+            DisplayHealing(healAmount, new Vector2(InitialPosition.x + Screen.width * 1 / 3, InitialPosition.y + 80));
+        }
     }
     void HealedByPriest()
     {
-        GameState.State.PlayerStatus.CurrentHealth += GameState.State.PlayerStatus.GuildStatus.Priest.GetPassiveStat();
-        if (GameState.State.PlayerStatus.GuildStatus.Priest.GetPassiveStat() > 0)
+        var healAmount = GameState.State.PlayerStatus.GuildStatus.Priest.GetPassiveStat();
+        if (healAmount > 0)
+        {
+            GameState.State.PlayerStatus.CurrentHealth += healAmount;
             DisplayHealingEffect();
+            DisplayHealing(healAmount, new Vector2(InitialPosition.x + Screen.width * 2 / 3, InitialPosition.y + 80));
+        }
     }
 }
