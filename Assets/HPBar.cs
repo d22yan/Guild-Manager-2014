5cc81e3bde058364614f416d6fe4107d37924424
Dann <yan4646@hotmail.com>
2014-07-09 13:45:35 -0400

    Include the new files

---
+++ Assets/HPBar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HPBar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    protected void DrawHPBar(float HPBarPercentage,GUISkin emptySkin, GUISkin fullSkin, Vector2 position, Vector2 size) {
+        GUI.skin = emptySkin;
+        GUI.BeginGroup(new Rect(position.x, position.y, size.x, size.y));
+        GUI.Box(new Rect(0, 0, size.x, size.y), Mathf.Round(HPBarPercentage * 100).ToString() + "%");
+        GUI.skin = fullSkin;
+        GUI.BeginGroup(new Rect(0, 0, size.x * HPBarPercentage, size.y));
+        GUI.Box(new Rect(0, 0, size.x, size.y), Mathf.Round(HPBarPercentage * 100).ToString() + "%");
+        GUI.EndGroup();
+        GUI.EndGroup();
+    }
+
+}
---
+++ Assets/ScalingGUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ScalingGUI {
+
+    private static float BaseDPI = 150;
+
+    public static bool Button(Rect rectangle, string value) 
+    {
+        Rect ScaledRectangle = new Rect(rectangle.x, rectangle.y, GetScalingFactor() * rectangle.width, GetScalingFactor() * rectangle.height);
+        return GUI.Button(rectangle, value);
+    }
+
+    public static float GetScalingFactor()
+    {
+        return (Screen.dpi == 0) ? 1 : Screen.dpi / BaseDPI;
+    }
+
+}
