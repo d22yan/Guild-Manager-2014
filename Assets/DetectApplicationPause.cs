cd1a98fa6750488e107275c2b89929ac1cd42fa0
Dann <yan4646@hotmail.com>
2014-07-19 15:26:12 -0400

    Testing OnApplicationPause();

---
+++ Assets/DetectApplicationPause.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectApplicationPause : MonoBehaviour {
+    public bool paused;
+    void OnGUI() {
+        if (paused)
+        {
+            GUI.Label(new Rect(100, 100, 50, 30), "Game paused");
+            GameState.State.PlayerGold = 0;
+        }
+    }
+    void OnApplicationPause(bool pauseStatus) {
+        Debug.Log("paused");
+        paused = pauseStatus;
+    }
+}
